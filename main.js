d7a97ca5b334a977350964a734e51ead34b30163
Rodrigo <rorro3025@outlook.com>
2020-10-22 22:41:41 -0500

    validacion_terminada

--- main.js
+++ main.js
@@ -12,11 +12,26 @@ f1.addEventListener('submit', (e) => {
 
     // objeto auth perteneciente a librerias de firebase 
 
-    auth.createUserWithEmailAndPassword(singup_email,singup_pass).then(userCredencial => {
-        f1.reset();
-        $('#singup-window').modal('hide');
-        console.log("registrado");
-    });
+    auth
+        .createUserWithEmailAndPassword(singup_email,singup_pass)
+        .then(userCredencial => {
+                f1.reset();
+                $('#singup-window').modal('hide');
+                console.log("registrado");
+            })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/email-already-in-use') {
+              alert('El correo ya a sido utilizado');
+              $('#singup-window').modal('hide');
+            } else {
+              alert(errorMessage);
+              $('#singup-window').modal('hide');
+            }
+            console.log(error);
+          });
 });
 
 // login
@@ -30,8 +45,51 @@ siginForm.addEventListener('submit', e => {
     
     // objeto auth perteneciente a librerias de firebase 
 
-    auth.signInWithEmailAndPassword(singin_email,singin_pass).then(function () {
-      $('#singin-window').modal('hide');
-      console.log("signin");
-    });
+    auth
+        .signInWithEmailAndPassword(singin_email,singin_pass)
+        .then(function () {
+                $('#singin-window').modal('hide');
+                alert("signin");
+               })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/user-not-found') {
+              alert('Usuario no registrado');
+              $('#singin-window').modal('hide');
+            } else {
+              alert(errorMessage);
+              $('#singin-window').modal('hide');
+            }
+            console.log(error);
+          });
+})
+
+// cerrar sesion 
+
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', e =>{
+    e.preventDefault();
+    auth
+        .signOut()
+        .then(() =>{
+            alert("Cerraste sesion");
+                });
+});
+
+// consulta BD 
+
+const ls = document.querySelector('.ls_alumnos');
+
+
+// eventos (sesion iniciada o no )
+
+auth.onAuthStateChanged(user =>{
+    if (user){
+        console.log("sesion on");
+    }else{
+        console.log("sesion cerrada");
+    }
 })
