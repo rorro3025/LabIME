a0eda415208bffa6649819559c9a0e640188c7f3
Rodrigo <rorro3025@outlook.com>
2020-11-03 17:16:54 -0600

    CRUD_FINISHED

--- main_2.js
+++ main_2.js
@@ -1,3 +1,6 @@
+// estado del formulario
+let status = false;
+let id_global = '';
 // prueba de conexion
 console.log("keep it taco");
 
@@ -15,24 +18,41 @@ form_prof.addEventListener('submit', async e => {
     const name_form = document.querySelector('#name_form_prof');
     const email_form = document.querySelector('#email_form_prof');
     const pass_form = document.querySelector('#pass_form_prof');
-    await save_teacher(name_form.value, email_form.value, pass_form.value);
-    console.log("name " + name_form.value + " pass " + pass_form.value + " email " + email_form.value);
+    if (!status) {
+        await save_teacher(name_form.value, email_form.value, pass_form.value);
+        console.log("Profesor(a) " + name_form.value + " registrado(a)");
+    }else{
+        await updateTeacher(id_global,{
+            Nombre: name_form.value,
+            Email: email_form.value,
+            Password: pass_form.value
+        });
+        status = false
+        form_prof["btn-action"].innerHTML = "Registrar";
+    }
     form_prof.reset();
     name_form.focus();
 })
-// impresion de datos
+// eliminar profesor 
+const deleteTeacher = id => fs.collection("teachers").doc(id).delete();
+// editar profesor 
+const getTeach = (id) => fs.collection("teachers").doc(id).get();
+// actualizar profesor
+const updateTeacher = (id,teacher_up) => fs.collection("teachers").doc(id).update(teacher_up);
+// mostar profesor
 const teachers_list = document.querySelector('.ls_teach');
 const setup_teach = data => {
     if (data.length) {
         let html = '';
         data.forEach((doc) => {
             const teacher = doc.data();
+            var id_bot = doc.id;
             const li = `
             <li class="list-group-item list-group-item-action">
               <h5>${teacher.Nombre}</h5>
               <p>${teacher.Email}</p>
-              <button class="btn btn-primary borrar_bot">Eliminar</button>
-              <button class="btn btn-secondary">Editar</button>
+              <button class="btn btn-primary borrar_bot" data-id="${id_bot}">Eliminar</button>
+              <button class="btn btn-secondary update_bot" data-id="${id_bot}">Editar</button>
             </li>
           `;
             html += li;
@@ -63,14 +83,38 @@ auth.onAuthStateChanged(user => {
     if (user) {
         onGetTeacher((querySnapshot) => {
             setup_teach(querySnapshot.docs);
-            var array_bot = document.querySelectorAll('.borrar_bot');
-            for (let index = 0; index < array_bot.length; index++) {
-                array_bot[index].addEventListener('click', () => {
-                    console.log(array_bot[index].innerHTML);
-                });
-            }
+            setListenersDelete();
+            setListenersUpdate();
         })
     } else {
         setup_teach([]);
     }
 })
+
+function setListenersDelete() {
+    var array_bot = document.querySelectorAll('.borrar_bot');
+    for (let index = 0; index < array_bot.length; index++) {
+        array_bot[index].addEventListener('click', () => {
+            deleteTeacher(array_bot[index].getAttribute('data-id'));
+            console.log("Se elimino dato");
+        });
+    }
+}
+
+function setListenersUpdate() {
+    var array_bot = document.querySelectorAll('.update_bot');
+    for (let index = 0; index < array_bot.length; index++) {
+        array_bot[index].addEventListener('click', async () => {
+            var id_num = array_bot[index].getAttribute('data-id');
+            var doc = await getTeach(id_num);
+            const teacher = doc.data();
+            form_prof['name_form_prof'].value = teacher.Nombre;
+            form_prof['email_form_prof'].value = teacher.Email;
+            form_prof['pass_form_prof'].value = teacher.Password;
+            status = true;
+            id_global = id_num;
+            alert(id_global);
+            form_prof["btn-action"].innerHTML = "actualizar";
+        });
+    }
+}
